b19d3204bfc5817af4527ca03a5500d99a15bcb2
妍娟 <smsh31041@gmail.com>
2026-05-27 23:13:09 +0800

    單字記數

---
+++ Vocabularynum/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Vocabularynum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("請輸入一段有意義的英文字句");
+            string input = Console.ReadLine();
+            string[] split = input.ToLower().Split(' ', StringSplitOptions.RemoveEmptyEntries);//空白去掉
+
+            Dictionary<string, int> words = new Dictionary<string, int>();
+            foreach (string word in split)
+            {
+                if (words.ContainsKey(word))
+                {
+                    words[word]++;
+                }
+                else
+                {
+                    words[word] = 1;
+                }
+            }
+            //逐一取出字典內的值
+            //var實際的型別是KeyValuePair<string, int>
+            foreach (var result in words)
+            {
+                Console.WriteLine($"{result.Key}: {result.Value}");
+            }
+
+        }
+    }
+}
